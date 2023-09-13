463ad91a8c5b77d56eb5f8dc1b149a65a66763c5
GBonacini <gabriele.bonacini@protonmail.com>
2023-09-13 15:34:54 +0200

    Cleaning

--- include/StringUtils.hpp
+++ include/StringUtils.hpp
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------
 // Tssh - A ssh test client. 
-// Copyright (C) 2016-2021  Gabriele Bonacini
+// Copyright (C) 2016-2023  Gabriele Bonacini
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
