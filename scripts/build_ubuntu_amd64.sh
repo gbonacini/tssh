0563042ce89d4aaaac04a09a59e03b429ebc28f4
GBonacini <gabriele.bonacini@protonmail.com>
2025-06-21 14:26:16 +0200

    Debugging: removed unnecessary actions

--- scripts/build_ubuntu_amd64.sh
+++ scripts/build_ubuntu_amd64.sh
@@ -24,3 +24,25 @@ Description: tssh is a ssh experimental client
 EOF
 
 fakeroot dpkg --build ./deb/tssh_ubuntu_amd64_22_04
+
+./configure WITH_LTO=yes
+make clean all
+
+mkdir -p ./deb/tssh_ubuntu_amd64_22_04_lto/usr/local/man/man1
+mkdir -p ./deb/tssh_ubuntu_amd64_22_04_lto/usr/local/bin
+mkdir    ./deb/tssh_ubuntu_amd64_22_04_lto/DEBIAN
+
+cp ./src/tssh   ./deb/tssh_ubuntu_amd64_22_04_lto/usr/local/bin
+cp ./doc/tssh.1 ./deb/tssh_ubuntu_amd64_22_04_lto/usr/local/man/man1
+
+cat > ./deb/tssh_ubuntu_amd64_22_04_lto/DEBIAN/control << EOF
+Package: tssh
+Version: ${VERSION}
+Maintainer: gbonacini
+Depends: libssl3 
+Architecture: amd64
+Homepage: https://github.com/gbonacini
+Description: tssh is a ssh experimental client
+EOF
+
+fakeroot dpkg --build ./deb/tssh_ubuntu_amd64_22_04_lto
